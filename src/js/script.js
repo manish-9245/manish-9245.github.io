fcbe158f2738cde597706f7ed2a9504c4ffd74ec
Manish Tiwari <69393822+manish-9245@users.noreply.github.com>
2023-07-27 15:10:15 +0530

    added coding stats

--- src/js/script.js
+++ src/js/script.js
@@ -22,10 +22,10 @@ function closeMenu() {
 const toggleSwitch = document.querySelector(
   '.theme-switch input[type="checkbox"]'
 );
-
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+
   } else {
     document.documentElement.setAttribute("data-theme", "light");
   }
@@ -38,10 +38,10 @@ toggleSwitch.addEventListener("change", switchTheme, false);
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark"); //add this
+    localStorage.setItem("theme", "dark");
   } else {
     document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light"); //add this
+    localStorage.setItem("theme", "light");
   }
 }
 
@@ -53,10 +53,13 @@ const currentTheme = localStorage.getItem("theme")
 
 if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
-
+  
   if (currentTheme === "dark") {
     toggleSwitch.checked = true;
+    
   }
+  
+  
 }
 
 //timeline
@@ -67,3 +70,102 @@ let myDate = document.querySelector("#datee");
 
 const yes = new Date().getFullYear();
 myDate.innerHTML = yes;
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('https://leetcode-stats-api.herokuapp.com/coderbabuaa')
+      .then(response => response.json())
+      .then(data => {
+          const totalSolvedElement = document.getElementById('totalSolved');
+          totalSolvedElement.textContent = data.totalSolved;
+
+          const totalQuestionsElement = document.getElementById('totalQuestions');
+          totalQuestionsElement.textContent = data.totalQuestions;
+
+          const easySolvedElement = document.getElementById('easySolved');
+          easySolvedElement.textContent = data.easySolved;
+
+          const totalEasyQuestionsElement=document.getElementById('totalEasy');
+          totalEasyQuestionsElement.textContent = data.totalEasy;
+
+          const mediumSolvedElement = document.getElementById('mediumSolved');
+          mediumSolvedElement.textContent = data.mediumSolved;
+
+          const totalMediumQuestionsElement=document.getElementById('totalMedium');
+          totalMediumQuestionsElement.textContent = data.totalMedium;
+
+          const hardSolvedElement = document.getElementById('hardSolved');
+          hardSolvedElement.textContent = data.hardSolved;
+
+          const totalHardQuestionsElement=document.getElementById('totalHard');
+          totalHardQuestionsElement.textContent = data.totalHard;
+
+          const totalSolved = data.totalSolved;
+            const totalQuestions = data.totalQuestions;
+
+            const progress = (totalSolved / totalQuestions) * 100;
+
+            // Create the circular progress bar using Chart.js
+            new Chart("progressChart", {
+              type: "doughnut",
+              data: {
+                  labels: [],
+                  datasets: [{
+                      data: [progress, 100 - progress],
+                      backgroundColor: ["#ff6384", "#ddd"],
+                  }]
+              },
+              options: {
+                  cutout: "80%",
+                  animation: {
+                      animateScale: true,
+                      animateRotate: true
+                  },
+                  legend: {
+                      display: false
+                  },
+                  tooltips: {
+                      enabled: false
+                  },
+                  plugins: {
+                    datalabels: {
+                        display: true,
+                        color: '#000',
+                        formatter: function(value, context) {
+                            return totalSolved + ' (' + progress.toFixed(2) + '%)';
+                        },
+                        font: {
+                            size: '18'
+                        }
+                    }
+                }
+            
+              }
+          });
+          const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+          const githubImageElement = document.getElementById('githubimage');
+      
+          function switchTheme(e) {
+              if (e.target.checked) {
+                  document.documentElement.setAttribute("data-theme", "dark");
+                  localStorage.setItem("theme", "dark");
+                  githubImageElement.setAttribute("src", "https://github-readme-streak-stats.herokuapp.com?user=manish-9245&theme=light&hide_border=true&background=FFFFFF");
+
+              } else {
+                  document.documentElement.setAttribute("data-theme", "light");
+                  localStorage.setItem("theme", "light");
+                  githubImageElement.setAttribute("src", "https://github-readme-streak-stats.herokuapp.com?user=manish-9245&theme=dark&hide_border=true&background=000000");
+
+              }
+          }
+      
+          toggleSwitch.addEventListener("change", switchTheme, false);
+      
+          const currentTheme = localStorage.getItem("theme") || "light";
+          document.documentElement.setAttribute("data-theme", currentTheme);
+          toggleSwitch.checked = currentTheme === "dark";
+      
+          if (currentTheme === "dark") {
+              githubImageElement.setAttribute("src", "https://github-readme-streak-stats.herokuapp.com?user=manish-9245&theme=dark&hide_border=true&background=000000");
+          }
+      })
+      .catch(error => console.log('Error fetching data:', error));
+});
